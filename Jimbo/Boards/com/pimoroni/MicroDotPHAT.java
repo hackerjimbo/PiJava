8cff1afb27cca496b4b3a43be9065508a9c50cc5
pi <pi@c0>
2016-09-01 10:49:54 +0100

    Added IS31FL3730 device and the Micro Dot pHAT board that uses it.

---
+++ Jimbo/Boards/com/pimoroni/MicroDotPHAT.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2016 Jim Darby.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package Jimbo.Boards.com.pimoroni;
+
+import com.pi4j.io.i2c.I2CBus;
+
+import Jimbo.Devices.Pi2C;
+import Jimbo.Devices.IS31FL3730;
+import java.io.IOException;
+
+/**
+ * This class interfaces to the Pimoroni Micro Dot pHAT. It consists of three
+ * IS31FL3730 controller chips wired to six displays in a delightfully unusual
+ * manner. I blame the grog!
+ * 
+ * @author Jim Darby
+ */
+public class MicroDotPHAT
+{
+    /**
+     * Constructor - builds an object to handle the device. Only one may be used
+     * at once.
+     * 
+     * @throws IOException In case of error
+     * @throws InterruptedException In case of error
+     */
+    public MicroDotPHAT () throws IOException, InterruptedException
+    {
+        final I2CBus bus = Pi2C.useBus ();
+    
+        for (int i = 0; i < driver.length; ++i)
+        {
+            driver[i] = new IS31FL3730 (bus, ADDRS[i]);
+            driver[i].setMode (IS31FL3730.MODE_M12 | IS31FL3730.MODE_8X8);
+            driver[i].setLightingEffect (0x0e);
+            driver[i].setPWM (128);
+            
+            for (int j = 0; j < 8; ++j)
+            {
+                m1[i][j] = 0;
+                m2[i][j] = 0;
+            }
+            
+            driver[i].fastUpdateM1 (m1[i]);
+            driver[i].fastUpdateM2 (m2[i]);
+            driver[i].update ();
+        }
+    }
+    
+    /**
+     * Set a single bit on the display. The origin is at the bottom left, as it
+     * should be.
+     * 
+     * @param x The x coordinate, range 0 to 29.
+     * @param y The y coordinate, range 0 to 6.
+     * @param on Make the pixel light up?
+     * @throws IOException In case of issues.
+     */
+    public void set (int x, int y, boolean on) throws IOException
+    {
+        // Validate parameters
+        if ( x< 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+            throw new IOException ("Invalid coordidinates");
+        
+        // Now figure out the display and the coordinates on it.
+        final int index = x / 10;
+        final boolean left = (x % 10) < 5;
+        final int offset = x % 5;
+        final int y_flip = HEIGHT - 1 - y;
+        
+        // Now map it into the device
+        if (left)
+        {
+            if (on)
+                m2[index][offset] |= (byte) (1 << y_flip);
+            else
+                m2[index][offset] &= (byte) ~(1 << y_flip);
+        }
+        else
+        {
+            if (on)
+                m1[index][y_flip] |= (byte) (1 << offset);
+            else
+                m1[index][y_flip] &= (byte) ~(1 << offset);
+        }
+    }
+    
+    /**
+     * Set a decimal point on the display.
+     * 
+     * @param n Which one. 0 is leftmost, 5 is rightmost.
+     * @param on Turn it on or off? True for on, false for off.
+     * @throws IOException In case of error.
+     */
+    public void setPoint (int n, boolean on) throws IOException
+    {
+        if (n < 0 || n > 5)
+            throw new IOException ("Invalid decimal point");
+        
+        // Now figure out the display and the coordinates on it.
+        final int index = n / 2;
+        final boolean left = (n % 2) == 0;
+
+        // Now map it into the device
+        if (left)
+        {
+            if (on)
+                m2[index][7] |= (byte) (0x40);
+            else
+                m2[index][7] &= (byte) ~(0x40);
+        }
+        else
+        {
+            if (on)
+                m1[index][6] |= (byte) 0x80;
+            else
+                m1[index][6] &= (byte) ~(0x80);
+        }
+    }
+    
+    /**
+     * Update the physical device.
+     * @throws IOException In case of problems.
+     */
+    public void update () throws IOException
+    {       
+        for (int i = 0; i < driver.length; ++i)
+        {
+            driver[i].fastUpdateM1 (m1[i]);
+            driver[i].fastUpdateM2 (m2[i]);
+            driver[i].update ();
+        }
+    }
+   
+    /**
+     * Set the PWM of the board.
+     * @param pwm Bright nice in range 0 to 128 (yes, 129 levels).
+     * @throws IOException In case of trouble.
+     */
+    public void setPWM (int pwm) throws IOException
+    {
+        // Rather naughtily we let the lower level routines do the range checking.
+        for (int i = 0; i < driver.length; ++i)
+            driver[i].setPWM (pwm);
+    }
+    
+    /**
+     * Basic test routine.
+     * @param args Command line arguments, ignored.
+     * @throws IOException In case of error.
+     * @throws InterruptedException In case of error.
+     */
+    public static void main (String[] args) throws IOException, InterruptedException
+    {
+        final MicroDotPHAT m = new MicroDotPHAT ();
+        boolean setting = true;
+        int pwm = 128;
+    
+        while (true)
+        {
+            for (int i = 0; i < 6; ++i)
+            {
+                m.setPoint (i, setting);
+                m.update ();
+                Thread.sleep (200);
+            }
+            
+            for (int y = 0; y < HEIGHT; ++y)
+                for (int x = 0; x < WIDTH; ++x)
+                {
+                    m.setPWM (pwm);
+
+                    pwm -= 1;
+
+                    if (pwm < 0)
+                        pwm = 128;
+            
+                    m.set (x, y, setting);
+                    m.update ();
+                    
+                    Thread.sleep (5);
+                }
+            
+            setting = !setting;
+        }
+    }
+    
+    /** The width of the device. */
+    public static final int WIDTH = 30;
+    /** The height of the device. */
+    public static final int HEIGHT = 7;
+    
+    /** The addresses, left to right, of the chips */
+    private static final int[] ADDRS = { 0x63, 0x62, 0x61 };
+    /** Our drivers for each chip. */
+    private final IS31FL3730 driver[] = new IS31FL3730[ADDRS.length];
+    
+    /** Local cache of M1 data. */
+    private final byte[][] m1 = new byte[driver.length][8];
+    /** Local cache of M2 data. */
+    private final byte[][] m2 = new byte[driver.length][8];
+}
---
+++ Jimbo/Devices/IS31FL3730.java
@@ -0,0 +1,314 @@
+/*
+ * Copyright (C) 2016 Jim Darby.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package Jimbo.Devices;
+
+import java.io.IOException;
+
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+
+/**
+ * This class drive the IS31FL3730 display controller. It's primary goal is to
+ * be generic so that its users can provide the appropriate parameters to make
+ * it work with the highly board specific implementation.
+ * 
+ * @author Jim Darby
+ */
+public class IS31FL3730 {
+    /**
+     * Constructor. This uses the default device address on the given bus.
+     * @param bus The bus the controller is on.
+     * @throws IOException When it can't create the bus device.
+     */
+    public IS31FL3730 (I2CBus bus) throws IOException
+    {
+        device = bus.getDevice (I2C_ADDR);
+        
+        init ();
+    }
+    
+    /**
+     * Constructor. The uses a specified address on the given bus,
+     * @param bus The bus the controller is on.
+     * @param addr The address on the bus
+     * @throws IOException When it can't create the bus device.
+     */
+    public IS31FL3730 (I2CBus bus, int addr) throws IOException
+    {
+        device = bus.getDevice (addr);
+        
+        init ();
+    }
+    
+    /**
+     * Set internal things up.
+     */
+    private void init ()
+    {
+        for (int i = 0; i < MAX_TRIES; ++i)
+            tries [i] = 0;
+    }
+    
+    /**
+     * Set the mode of the device.
+     * @param mode You can have (or not) MODE_SSD, MODE_AE and one of:
+     * MODE_8X8, MODE_7X9, MODE_6X10 or MODE_9X11 and one of:
+     * MODE_M1, MODE_M2 or MODE_M12.
+     * @throws IOException If anything goes wrong.
+     */
+    public void setMode (int mode) throws IOException
+    {
+        // Did they set any naughty bits?
+        if ((mode & ~(0x9f)) != 0)
+            throw new IOException ("Bad mode value " + mode);
+        
+        retryWrite (REG_CONFIG, (byte) mode);
+    }
+    
+    /**
+     * Set a single byte in M1.
+     * @param offset The offset (0 to 10).
+     * @param value The value
+     * @throws java.io.IOException On invalid parameters or error.
+     */
+    public void setM1 (byte offset, byte value) throws IOException
+    {
+        if (offset < 0 || offset > 10)
+            throw new IOException ("Invalid offset " + offset);
+        
+        retryWrite (REG_M1_BASE + offset, value);
+    }
+ /**
+     * Set a single byte in M2.
+     * @param offset The offset (0 to 10).
+     * @param value The value
+     * @throws java.io.IOException On invalid parameters or error.
+     */
+    public void setM2 (byte offset, byte value) throws IOException
+    {
+        if (offset < 0 || offset > 10)
+            throw new IOException ("Invalid offset " + offset);
+        
+        retryWrite (REG_M2_BASE + offset, value);
+    }
+    
+    /**
+     * Write the M1 matrix data and then (maybe) hit the update register.
+     * This needs between 1 to 12 bytes of data but the 12th (if there)
+     * isn't used for anything useful except triggering the device to update.
+     * @param data A 1- to 12-item byte array.
+     * @throws IOException In case of problems.
+     */
+    public void fastUpdateM1 (byte data[]) throws IOException
+    {
+        if (data.length < 1 || data.length > 12)
+            throw new IOException ("Invalid fastUpdate data");
+        
+        retryWrite (REG_M1_BASE, data, 0, data.length);
+    }
+    
+    /**
+     * Write the entire M2 matrix data.
+     * @param data A 1- to 11-item byte array.
+     * @throws IOException In case of problems.
+     */
+    public void fastUpdateM2 (byte data[]) throws IOException
+    {
+        if (data.length < 1 || data.length > 11)
+            throw new IOException ("Invalid fastUpdate data");
+        
+        retryWrite (REG_M2_BASE, data, 0, data.length);
+    }
+
+    /**
+     * Set the lighting effect register. See the datasheet for the full details.
+     * @param effect The value to use. Must be between 0 and 127 inclusive.
+     * @throws IOException In case of error.
+     */
+    public void setLightingEffect (int effect) throws IOException
+    {
+        if (effect < 0 || effect > 127)
+            throw new IOException ("Invalid effect " + effect);
+        
+        retryWrite (REG_LER, (byte) effect);
+    }
+    
+    /**
+     * Set the PWM parameters.
+     * @param pwm The PWM value. Must be between 0 and 128 inclusive.
+     * @throws IOException In case of error.
+     */
+    public void setPWM (int pwm) throws IOException
+    {
+        if (pwm < 0 || pwm > 128)
+            throw new IOException ("Bad PWM " + pwm);
+        
+        retryWrite (REG_PWM, (byte) pwm);
+    }
+    
+    /**
+     * Tell the device to update its display.
+     * @throws IOException In case things go wrong.
+     */
+    public void update () throws IOException
+    {
+        retryWrite (REG_UCR, (byte) 0x00);
+    }
+    
+    /**
+     * Try a bus write until it works.
+     * @param reg The register to write.
+     * @param value The value to write
+     * @throws IOException If it really can't be made to work!
+     */
+    private void retryWrite (int reg, byte value) throws IOException
+    {
+        IOException error = null;
+        
+        total += 1;
+        
+        for (int i = 0; i < MAX_TRIES; ++i)
+        {
+            try {
+                device.write (reg, value);
+                                
+                if (i > 0) {
+                    ++tries[i-1];
+                    printTries ();
+                }
+                
+                error = null;
+                break;
+            }
+            
+            catch (IOException e) {
+                error = e;
+            }
+        }
+        
+        if (error != null)
+            throw error;
+    }
+    
+    /**
+     * Try a bus write until it works.
+     * @param reg The register to write.
+     * @param value The values to write
+     * @throws IOException If it really can't be made to work!
+     */
+    private void retryWrite (int reg, byte[] value, int base, int length) throws IOException
+    {
+        IOException error = null;
+                
+        total += 1;
+                
+        for (int i = 0; i < MAX_TRIES; ++i)
+        {
+            try {
+                device.write (reg, value, base, length);
+                
+                if (i > 0) {
+                    ++tries[i-1];
+                    printTries ();
+                }
+                
+                error = null;
+                break;
+            }
+            
+            catch (IOException e) {
+                error = e;
+            }
+            
+            try {
+                Thread.sleep (1);
+            }
+            
+            catch (Exception e) {
+            }
+        }
+        
+        if (error != null)
+            throw error;
+    }
+    
+    /**
+     * Print out diagnostic information in case of failure.
+     */
+    private void printTries ()
+    {
+        int extra = 0;
+        
+        for (int i = 0; i < MAX_TRIES; ++i)
+            if (tries[i] != 0)
+            {
+                extra += tries[i] * (i + 1);
+                System.out.println ("Tries " + (i + 1) + " = " + tries[i]);
+            }
+        
+        System.out.println ("As as percentage: " + (((double) extra / total)) * 100);
+    }
+    
+    /** The maximum number of tries to send something over the bus. */
+    private static final int MAX_TRIES = 20;
+    /** Create a histogram of the number of tries;. */
+    private final int tries[] = new int[MAX_TRIES];
+    /** Total number of transmissions attempted. */
+    private long total = 0;
+    
+    /** The default I2C address of the device. */
+    private static final byte I2C_ADDR = 0x60;
+    
+    /** Configuration register. */
+    private static final byte REG_CONFIG = 0x00;
+    /** Base of Matrix 1 data. */
+    private static final byte REG_M1_BASE = 0x01;
+    /** Base of Matrix 2 data. */
+    private static final byte REG_M2_BASE = 0x0e;
+    /** Update control register. */
+    private static final byte REG_UCR = 0x0c;
+    /** Lighting effects register. */
+    private static final byte REG_LER = 0x0d;
+    /** PWM register. */
+    private static final byte REG_PWM = 0x19;
+    /** Reset register */
+    private static final byte REG_RESET = (byte) 0xff;
+    
+    /** Software shutdown mode */
+    public static final byte MODE_SSD = (byte) 0x80;
+    /** Set matrix 1 only */
+    public static final byte MODE_M1 = 0x00;
+    /** Set matrix 2 only */
+    public static final byte MODE_M2 = 0x08;
+    /** Set matrix 1 and 2 */
+    public static final byte MODE_M12 = 0x18;
+    /** Audio enable */
+    public static final byte MODE_AE = 0x04;
+    /** Set 8x8 */
+    public static final byte MODE_8X8 = 0x00;
+    /** Set 7*9 */
+    public static final byte MODE_7X9 = 0x01;
+    /** Set 6*10 */
+    public static final byte MODE_6X10 = 0x02;
+    /** Set 5*11 */
+    public static final byte MODE_5X11 = 0x03;
+   
+    /** Point to the device we're using. */
+    private final I2CDevice device;
+}

7d32eba56bc2528740ba6353a86915540c547caf
Jim Darby <uberscubajim@gmail.com>
2017-03-29 00:34:55 +0100

    Added some more methods to make it a more general case.

--- Jimbo/Graphics/ColourMatrix.java
+++ Jimbo/Graphics/ColourMatrix.java
@@ -19,7 +19,10 @@
 package Jimbo.Graphics;
 
 /**
- * This interface describes a matrix of RGB Pixels.
+ * This interface describes a matrix of RGB Pixels. Note that at least one of
+ * setPixel (Point p, int r, int g, int b) and
+ * setPixel (int x, int y, int r, int g, int b) must be defined to avoid
+ * infinite recursion from the default implementations.
  * 
  * @author Jim Darby
  */
@@ -33,18 +36,45 @@ public interface ColourMatrix extends Matrix
      * @param g The green value: 0 to 255.
      * @param b The blue value: 0 to 255.
      */
-    public void setPixel (Point p, int r, int g, int b);
+    default public void setPixel (Point p, int r, int g, int b)
+    {
+        setPixel (p.getX (), p.getY (), r, g, b);
+    }
     
+     /**
+     * Sets a pixel to a specific colour.
+     * 
+     * @param x The X coordinate of the pixel.
+     * @param y The Y coordinate of the pixel.
+     * @param r The red value: 0 to 255.
+     * @param g The green value: 0 to 255.
+     * @param b The blue value: 0 to 255.
+     */
+    default public void setPixel (int x, int y, int r, int g, int b)
+    {
+        setPixel (new Point (x, y), r, g, b);
+    }
+
     /**
      * Set a specific pixel to a specific Colour value.
      * 
      * @param p The point to set.
      * @param c The colour to set it to.
      */
-    public void setPixel (Point p, Colour c);
+    default public void setPixel (Point p, Colour c)
+    {
+        setPixel (p, c.getRed (), c.getGreen (), c.getBlue ());
+    }
     
     /**
-     * Update the display.
+     * Sets a pixel to a specific colour.
+     * 
+     * @param x The X coordinate of the pixel.
+     * @param y The Y coordinate of the pixel.
+     * @param c The colour to set it to.
      */
-    public void show ();
+    default public void setPixel (int x, int y, Colour c)
+    {
+        setPixel (x, y, c.getRed (), c.getGreen (), c.getBlue ());
+    }
 }

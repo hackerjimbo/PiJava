0756462126bf52f9a7c017db7cd1c1b55cbe7364
Jim Darby <uberscubajim@gmail.com>
2017-04-04 21:55:52 +0100

    Use generic interface.

--- Jimbo/Graphics/MonoMatrixDemo.java
+++ Jimbo/Graphics/MonoMatrixDemo.java
@@ -40,18 +40,46 @@ public class MonoMatrixDemo
         final Point limits = m.getMax ();
         final int max_x = limits.getX ();
         final int max_y = limits.getY ();
+        final Double on = new Double (1);
+        final Double off = new Double (0);
         
+        for (int y = 0; y <= max_y; ++y)
+            for (int x = 0; x <= max_x; ++x)
+            {
+                final Point p = new Point (x, y);
+                m.setPixel (p, on);
+                m.show ();
+                Thread.sleep (50);
+                m.setPixel (p, off);
+                m.show ();
+            }
+        
+        final double max_distance = Math.sqrt (max_x * max_x + max_y * max_y);
+        
+        int phase = 0;
+                
         while (true)
         {
             for (int y = 0; y <= max_y; ++y)
                 for (int x = 0; x <= max_x; ++x)
                 {
-                    m.setPixel (x, y, 255);
-                    m.show ();
-                    Thread.sleep (50);
-                    m.setPixel (x, y, 0);
-                    m.show ();
+                    final double distance = Math.sqrt (x*x + y*y);
+                    final double fraction = 1 - distance / max_distance;
+                    final double value = Math.sin ((fraction + phase / 100.0) * 2 * Math.PI);
+                    final Double pixel = new Double (0.5 + 0.5 * value);
+                    final Point p = new Point (x, y);
+                    
+                    m.setPixel (p, pixel);
                 }
+            
+            m.show ();
+            
+            phase += 1;
+            
+            if (phase == 100)
+                phase = 0;
+            
+            Thread.sleep (10);
         }
     }
 }
